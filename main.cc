875b03a3d2df35a0de2a1629913a40a88009aed3
LeiZhang-Hunter <357733652@qq.com>
2021-04-12 14:29:18 +0800

    sms4加解密工具

--- main.cc
+++ main.cc
@@ -14,10 +14,8 @@ using namespace std;
 int main(int argc,char* argv[])
 {
     //设置初始密码（16位）
-    string strPasswd="1000000ACCOUNT#@";
     sms4_key_t key;
     //设置初始化向量（16位）
-    string strInitVec = "462_V20000000000";
     std::shared_ptr<src::EncryptWorker> worker = std::make_shared<src::EncryptWorker>();
     worker->setCommandArgc(argc);
     worker->setCommandArgv(argv);
